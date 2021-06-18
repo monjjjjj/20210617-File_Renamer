4d938893f729f1d983eb7c2b3a0a7163448389ac
monjjjjj <monjjjj526@gmail.com>
2021-06-18 16:09:17 +0800

    Try something about regex

--- main.py
+++ main.py
@@ -22,8 +22,23 @@ print(a.group())
 # print(b.group())
 # print(c.group())
 print(d.group())
-
-string = "臺中市南屯區埔興段35-12地號"
-regex = re.compile(r"段(\d+-*\d*)")
+###################################################################
+string = "Chloe_File_1"
+regex = re.compile(r"([A-Z]).*_([A-Z]).*_(\d)")
 match = regex.search(string)
-print(match.group(1))
+match2 = regex.findall(string)
+# match = re.compile(r"段(\d+-*\d*)").search(string)
+print(match.group(1, 2, 3))
+print(match2)
+###################################################################
+string2 = "今天的日期是：2020-07-01 14:02:48"
+
+time = re.search(r'\d+-\d+-\d+ \d+:\d+:\d+', string2).group()
+print(time)
+###################################################################
+regex1 = re.compile(r"(\w)(\W)")
+find = regex1.findall("A/1$5&6")
+print(find)
+
+for match in regex1.finditer("A/1$5&"):
+    print(match.group(1), match.group(2))
