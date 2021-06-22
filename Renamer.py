4ff26d799330ac3ef4d4e8175ad28f1cc1be1656
monjjjjj <monjjjj526@gmail.com>
2021-06-23 00:11:06 +0800

    Add the function of renamer

--- Renamer.py
+++ Renamer.py
@@ -1,9 +1,36 @@
 import re
 import glob
 import os
-file = glob.glob(r"/Users/chloe/Desktop/20210617 File_Renamer/*.xlsx")
+import os.path
+
+#file = glob.glob(r"/Users/chloe/Desktop/20210617 File_Renamer/*.xlsx")
+
+#for filename in file:
+    #new_name = re.sub(r"(/Users/chloe/Desktop/20210617 File_Renamer/)([A-Z]).*_([A-Z]).*_(\d)", r"\g<1>\g<2>_\g<3>_\g<4>", filename)
+    # print(filename, new_name)
+    # os.rename(filename, new_name)
+
+
+def folder_path():
+    while True:
+        path = input("The path of files: ")
+        if os.path.isdir(path):
+            return str(path)
+        else:
+            print("The path does not exist!")
+
+
+directory = "/Users/chloe/Desktop/20210617 File_Renamer/"
+
+file = glob.glob(r"%s/*" % directory)
+print(file)
+
+pattern = input("The pattern: ")
+replace = input("The replacement:")
+
 
 for filename in file:
-    new_name = re.sub(r"(/Users/chloe/Desktop/20210617 File_Renamer/)([A-Z]).*_([A-Z]).*_(\d)", r"\1\2_\3_\4", filename)
-    print(filename, new_name)
-    os.rename(filename, new_name);
+    new_name = re.sub(r"({0}){1}".format(directory, pattern), r"{0}".format(replace), filename)
+    print(r"({0}){1}".format(directory, pattern), r"{0}".format(replace), filename)
+    print(new_name)
+    os.rename(filename, new_name)
