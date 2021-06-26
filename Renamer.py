95333fc8299da22c8a7b86d93b5633d1ed147a68
monjjjjj <monjjjj526@gmail.com>
2021-06-26 22:06:06 +0800

    Use system arguments in python git status clear

--- Renamer.py
+++ Renamer.py
@@ -2,13 +2,15 @@ import re
 import glob
 import os
 import os.path
+import sys
 
-#file = glob.glob(r"/Users/chloe/Desktop/20210617 File_Renamer/*.xlsx")
 
-#for filename in file:
-    #new_name = re.sub(r"(/Users/chloe/Desktop/20210617 File_Renamer/)([A-Z]).*_([A-Z]).*_(\d)", r"\g<1>\g<2>_\g<3>_\g<4>", filename)
-    # print(filename, new_name)
-    # os.rename(filename, new_name)
+# file = glob.glob(r"/Users/chloe/Desktop/20210617 File_Renamer/*.xlsx")
+
+# for filename in file:
+# new_name = re.sub(r"(/Users/chloe/Desktop/20210617 File_Renamer/)([A-Z]).*_([A-Z]).*_(\d)", r"\g<1>\g<2>_\g<3>_\g<4>", filename)
+# print(filename, new_name)
+# os.rename(filename, new_name)
 
 
 def folder_path():
@@ -20,17 +22,17 @@ def folder_path():
             print("The path does not exist!")
 
 
-directory = folder_path()
+if __name__ == "__main__":
 
-file = glob.glob(r"%s/*" % directory)
-# print(file)
+    directory = folder_path()
+    file = glob.glob(r"%s/*" % directory)
+    # print(file)
 
-pattern = input("The pattern: ")
-replace = input("The replacement:")
+    # pattern = input("The pattern: ")
+    # replace = input("The replacement:")
 
-
-for filename in file:
-    new_name = re.sub(r"({0}/){1}".format(directory, pattern), r"{0}".format(replace), filename)
-    print(r"({0}){1}".format(directory, pattern), r"{0}".format(replace), filename)
-    print(new_name)
-    os.rename(filename, new_name)
+    for filename in file:
+        new_name = re.sub(r"({0}/){1}".format(directory, sys.argv[0]), r"{0}".format(sys.argv[1]), filename)
+        print(r"({0}){1}".format(directory, sys.argv[0]), r"{0}".format(sys.argv[1]), filename)
+        print(new_name)
+        os.rename(filename, new_name)
