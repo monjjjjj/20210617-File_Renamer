e0b0588208f075e2db93cd0704160b538373de9c
monjjjjj <monjjjj526@gmail.com>
2021-06-27 17:15:11 +0800

    Use sys.argv correctly

--- Renamer.py
+++ Renamer.py
@@ -7,32 +7,30 @@ import sys
 
 # file = glob.glob(r"/Users/chloe/Desktop/20210617 File_Renamer/*.xlsx")
 
-# for filename in file:
-# new_name = re.sub(r"(/Users/chloe/Desktop/20210617 File_Renamer/)([A-Z]).*_([A-Z]).*_(\d)", r"\g<1>\g<2>_\g<3>_\g<4>", filename)
-# print(filename, new_name)
-# os.rename(filename, new_name)
-
-
-def folder_path():
-    while True:
-        path = input("The path of files: ")
-        if os.path.isdir(path):
-            return str(path)
-        else:
-            print("The path does not exist!")
-
+# for filename in file: new_name = re.sub(r"(/Users/chloe/Desktop/20210617 File_Renamer/)([A-Z]).*_([A-Z]).*_(\d)",
+# r"\g<1>\g<2>_\g<3>_\g<4>", filename) print(filename, new_name) os.rename(filename, new_name)
 
 if __name__ == "__main__":
 
-    directory = folder_path()
-    file = glob.glob(r"%s/*" % directory)
+    def folder_path():
+        while True:
+            path = input("The path of files: ")
+            if os.path.isdir(path):
+                return str(path)
+            else:
+                print("The path does not exist!")
+
+
+    direct = sys.argv[1]
+    # directory = folder_path(direct)
+    file = glob.glob(r"%s/*" % direct)
     # print(file)
 
     # pattern = input("The pattern: ")
     # replace = input("The replacement:")
 
     for filename in file:
-        new_name = re.sub(r"({0}/){1}".format(directory, sys.argv[0]), r"{0}".format(sys.argv[1]), filename)
-        print(r"({0}){1}".format(directory, sys.argv[0]), r"{0}".format(sys.argv[1]), filename)
+        new_name = re.sub(r"({0}/){1}".format(direct, sys.argv[2]), r"{0}".format(sys.argv[3]), filename)
+        print(r"({0}){1}".format(direct, sys.argv[2]), r"{0}".format(sys.argv[3]), filename)
         print(new_name)
         os.rename(filename, new_name)
