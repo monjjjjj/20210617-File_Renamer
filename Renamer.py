bf1269c60898986ee1cad5aae628c0b4b9c1b1fd
monjjjjj <monjjjj526@gmail.com>
2021-06-29 00:12:03 +0800

    fix the issue of absolue/relative directory.(line15)

--- Renamer.py
+++ Renamer.py
@@ -11,9 +11,28 @@ if __name__ == "__main__":
     if not os.path.isdir(directory):
         print("The path does not exist!")
     else:
-        os.chdir(r"%s" % directory)
-        files = os.listdir(directory)
+        os.chdir(directory)
+        files = os.listdir()
         for filename in files:
             new_name = re.sub(r"{0}".format(pattern), r"{0}".format(replace), filename)
             print(filename, '>>', new_name)
             os.rename(filename, new_name)
+
+
+# 1F
+# os.chdir('7F-1') # John goes to 7F-1 from 1F
+# files = os.listdir('7F-1') # John takes a picture of 7F-1 from 1F
+
+# os.chdir('7F-1') # John goes to 7F-1 from 1F
+# files = os.listdir() # John takes a picture
+
+#########################################################################
+
+# 7F
+# os.chdir('-1') # John goes to room 1
+# files = os.listdir('-1') # John takes a picture of room 1 of this room
+
+# os.chdir('-1') # John goes to 7F-1 from 1F
+# files = os.listdir() # John takes a picture
+
+
