47eeeeb9543abdc2668c3331492e4b66178c138c
monjjjjj <monjjjj526@gmail.com>
2021-06-29 23:33:33 +0800

    python argument values help message.

--- Renamer.py
+++ Renamer.py
@@ -1,20 +1,35 @@
+import argparse
 import re
 import os.path
-import sys
 
-if __name__ == "__main__":
 
-    directory = sys.argv[1]
-    pattern = sys.argv[2]
-    replace = sys.argv[3]
+def get_parser():
+    parser = argparse.ArgumentParser(description='Just a test!')
+    parser.add_argument("directory", help="Directory of files.")
+    parser.add_argument("pattern",
+                        help="Please input the regular expression of the files. "
+                             "Please refer to the following website."
+                             "https://docs.python.org/3/library/re.html")
+    parser.add_argument("replacement", help="Please input the group of replacement.")
+    return parser
 
-    if not os.path.isdir(directory):
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    direct = args.directory
+    patt = args.pattern
+    replace = args.replacement
+
+    print(direct)
+
+    if not os.path.isdir(direct):
         print("The path does not exist!")
     else:
-        os.chdir(directory)
-        files = os.listdir()
+        os.chdir(direct)
+        files = os.listdir(".")    # dot = current directory
         for filename in files:
-            new_name = re.sub(r"{0}".format(pattern), r"{0}".format(replace), filename)
+            new_name = re.sub(r"{0}".format(patt), r"{0}".format(replace), filename)
             print(filename, '>>', new_name)
             os.rename(filename, new_name)
 
@@ -34,5 +49,3 @@ if __name__ == "__main__":
 
 # os.chdir('-1') # John goes to 7F-1 from 1F
 # files = os.listdir() # John takes a picture
-
-
